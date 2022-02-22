517ad01cf9500a31deffd16958b1f29c3bbc8e65
davidromanl <roman.david@gmail.com>
2022-02-22 01:49:17 -0500

    Main

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const port = 3000
 
 let token,datos
 
-getToken = () => ({
-})
+getToken = () => {
+  const url = 'https://accounts.zoho.com/oauth/v2/token?refresh_token=1000.13dacb8ef678f3f3f1fe07d4a8dc2b30.b4f47af109e6be5915643f577d43c1b6&client_id=1000.8LI2BEFOUJBPRBR73KVRECD20K449P&client_secret=bcb4ac38c0f937c6531a0fa88b837bc110736fde70&grant_type=refresh_token'
+  axios.post(url)
+  .then(function({data}) {
+    token = data.access_token
+  })
+  .catch(function(err) {
+    console.error(err)
+  })
+}
 
-getData = () => ({
-axios.get('/')
+getData = () => {
+  url = 'https://creator.zoho.com/api/v2/voy.cel/Colombia/report/Productos_Ciclismo'
+axios.get('/',{ headers: { Authorization: 'Bearer '+token}})
+.then(function ({data}) {
+datos = data 
 })
+.catch(function(err) {
+  console.error(err)
+  getToken()
+})
+}
 
 app.get('/zohoapi', (req, res) => {
-  getData()
-
+  getData();
 res.json(datos)
 })
 
